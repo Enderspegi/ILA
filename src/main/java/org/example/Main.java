2388c24d96896aca0cae10cd0efd43a1d70cc0ca
zegi123 <enderspegi@gmail.com>
2025-10-21 08:20:27 +0200

    Beispiele für push und isEmpty

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        // Erstellung eines Stacks für String-Elemente
+        Stack<String> stack = new Stack<>();
+
+        System.out.println("Ist der Stack leer? " + stack.isEmpty()); // Ausgabe: true
+
+        // 1. push(item)
+        stack.push("Element 1 (unten)");
+        stack.push("Element 2 (mitte)");
+        stack.push("Element 3 (oben)");
 
     }
 }
